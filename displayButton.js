d5f11b3c97ca57d9417e15965f3596f801362d4c
Sneha-mrk <108858411+Sneha-mrk@users.noreply.github.com>
2022-07-10 19:32:11 +0530

    display Button
    display Button

---
+++ displayButton.js
@@ -0,0 +1,10 @@
+function hello(){
+	
+	var a=document.getElementById('n').value;
+	document.getElementById('m').value=a;
+	console.log(a);
+	
+}
+function clr(){
+	document.getElementById('n').value='';	
+}
