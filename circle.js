7069de52ae0aae4499f7a006923d994a6feb1beb
Sneha-mrk <108858411+Sneha-mrk@users.noreply.github.com>
2022-07-10 22:26:56 +0530

    Circle
    Circle

---
+++ circle.js
@@ -0,0 +1,7 @@
+function circle(){
+	var p=document.getElementById('t1').value;
+	var are=3.14*parseInt(p)*parseInt(p)
+	document.getElementById('t2').value=are;
+	var cir=2*3.14*parseInt(p)
+	document.getElementById('t3').value=cir;
+}
